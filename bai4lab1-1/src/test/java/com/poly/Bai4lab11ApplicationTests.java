038b140120886b5a22a71bd00e85c8ce6cf679c8
kietltpc <145770439+kietltpc@users.noreply.github.com>
2024-05-11 08:54:20 +0700

    indexjnjnnk

---
+++ bai4lab1-1/src/test/java/com/poly/Bai4lab11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bai4lab11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
